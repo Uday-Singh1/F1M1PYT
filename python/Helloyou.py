db476aeb461a1555cde9d0ede2786440ffc09887
test <test@tests-MacBook-Pro.local>
2021-09-17 10:54:05 +0200

    dit is mijn eerste commit

---
+++ python/Helloyou.py
@@ -0,0 +1,19 @@
+import datetime
+
+naam = input("Je naam ")
+print("ik ben " + naam)
+
+x = datetime.datetime.now()
+
+print(x)
+
+print (naam + " wil jij dit programma nog een keer doen ?")
+
+print("Y/N")
+yn = input (" ")
+
+#yn == Y
+    #if
+
+yn != Y
+    if print
